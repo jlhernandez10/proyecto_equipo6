813b5f88b5cb956345964e777e641b43bbf0a561
jlhernandez10 <jlsolucionesweb@gmail.com>
2021-11-21 21:04:17 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_equipo6/src/app.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Registro de usuarios',
+      home: RegistroForm(),
+    );
+  }
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegistroForm extends StatefulWidget {
+  RegistroForm({Key? key}) : super(key: key);
+
+  @override
+  _RegistroFormState createState() => _RegistroFormState();
+}
+
+class _RegistroFormState extends State<RegistroForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
