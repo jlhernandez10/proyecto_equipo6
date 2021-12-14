f7a0cc8bbe12f57cfe01484232027d9a82fb3cdb
jlhernandez10 <jlsolucionesweb@gmail.com>
2021-12-13 21:11:47 -0500

    Se han realizado pruebas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:proyecto_equipo6/main.dart';
+import 'package:proyecto_equipo6/pages/login.dart';
 
 void main() {
-  //testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  // Build our app and trigger a frame.
-  //await tester.pumpWidget(new SprintApp());
-
-  // Verify that our counter starts at 0.
-  //expect(find.text('0'), findsOneWidget);
-  //expect(find.text('1'), findsNothing);
-
-  // Tap the '+' icon and trigger a frame.
-  //await tester.tap(find.byIcon(Icons.add));
-  //await tester.pump();
-
-  // Verify that our counter has incremented.
-  //expect(find.text('0'), findsNothing);
-  //expect(find.text('1'), findsOneWidget);
-  //});
+  testWidgets('Test para probar la aplicación', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: LoginPage(),
+    )));
+    // Tap the '+' icon and trigger a frame.
+    //await tester.tap(find.byIcon(Icons.add));
+    //await tester.pump();
+  });
 }
